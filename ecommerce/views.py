9854026818f3e566b8aa62d7f14efdbacb459582
Chung Nguyen <zrmedia9@gmail.com>
2018-02-27 22:07:09 +0700

    first commit

---
+++ ecommerce/views.py
@@ -0,0 +1,88 @@
+# username: admin - password: q1w2e3r4t5
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
+from .forms import ContactForm, LoginForm
+
+def home_page(request):
+    context = {
+        "title": "Hello world",
+        "content": "Welcome to home page"
+    }
+    return render(request, 'home_page.html', context)
+
+def contact_page(request):
+    contact_form = ContactForm(request.POST or None)
+    context = {
+        "title": "Contact page",
+        "content": "Welcome to contact page",
+        "form": contact_form
+    }
+    if contact_form.is_valid():
+        print(contact_form.cleaned_data)
+    # if request.method == "POST":
+    #     print(request.POST)
+    #     print(request.POST.get('fullname'))
+
+    return render(request, 'contact/view.html', context)
+
+def login_page(request):
+    form = LoginForm(request.POST or None)
+    context = {
+        "form": form
+    }
+    print(request.user.is_authenticated())
+    if form.is_valid():
+        print(form.cleaned_data)
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
+        user = authenticate(request, username=username, password=password)
+        print(request.user.is_authenticated())
+        if user is not None:
+            login(request, user)
+            context['form'] = LoginForm()
+            return redirect("/login")
+        else:
+            print("Err")
+    return render(request, "auth/login.html", context)
+
+def register_page(request):
+    form = LoginForm(request.POST or None)    
+    if form.is_valid():
+        print(form.cleaned_data)
+    return render(request, "auth/register.html", {})
+
+def about_page(request):
+    context = {
+        "title": "About page",
+        "content": "Welcome to about page"
+    }
+    return render(request, 'home_page.html', context)
+
+def home_page_old(request):
+    html_ = """
+    <!doctype html>
+    <html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+        <title>Hello, world!</title>
+    </head>
+    <body>
+        <h1>Hello, world!</h1>
+
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    </body>
+    </html>
+    """
+    return HttpResponse(html_)
